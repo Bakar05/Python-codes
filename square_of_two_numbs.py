270e5e9a6829e2bb4f9a7fc1071a748b7df3e65b
Abu Bakar Imran <abubakar5feb2005@gmail.com>
2025-08-19 19:32:53 +0500

    Create square_of_two_numbs.py

---
+++ square_of_two_numbs.py
@@ -0,0 +1,8 @@
+def main():
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+    result = (a ** 2) - (b ** 2)
+    print(f"a^2 - b^2 = {result}")
+
+if __name__ == "__main__":
+    main()
